c07920b237cff358a9479c3328b92b9f7e24db8c
grefl <grefl@protonmail.com>
2021-07-16 20:34:55 +0200

    add better fallback

--- my_python_libs/get_file.py
+++ my_python_libs/get_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import urllib.error
 import urllib.parse
 import urllib.request
@@ -29,18 +30,29 @@ class Request():
     except urllib.error.HTTPError as e:
       print(e)
 
+def fetch_and_write():
+    url = None
+    file_name = None
+    args_len = len(sys.argv)
+    if args_len == 1:
+        print('please provide a url')
+        return
+    elif args_len == 2:
+            url = sys.argv[1]
+    elif args_len == 3:
+            url, file_name = sys.argv[1]
+
+    end = ''.join(url.split('.com')[1].split('/blob'))
+    new_url = raw_url + end
+    print(end)
+    req = Request()
+    headers = {'Accept': 'application/json' }
+    options = {'data': None, 'headers': headers}
+    file_name = file_name if file_name else url.split('/').pop()
+    res = req.get(new_url,options) 
+    with open(f"./{file_name}", 'w+') as f:
+      f.write(res)
+      print(f'finished writing {file_name}')
 
 
-url, file_name = sys.argv[1:] 
-
-end = ''.join(url.split('.com')[1].split('/blob'))
-new_url = raw_url + end
-print(end)
-req = Request()
-headers = {'Accept': 'application/json' }
-options = {'data': None, 'headers': headers}
-
-res = req.get(new_url,options) 
-with open(f'./{file_name}', 'w+') as f:
-  f.write(res)
-  print(f'finished writing {file_name}')
+fetch_and_write()
