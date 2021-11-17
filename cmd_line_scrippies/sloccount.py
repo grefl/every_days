9db925dce543fa0b26ad6731c1e2b52817f280f1
grefl <grefl@protonmail.com>
2021-11-17 11:48:48 +0100

    Add source lines of code count

---
+++ cmd_line_scrippies/sloccount.py
@@ -0,0 +1,16 @@
+import sys
+from pathlib import Path
+
+def main():
+    file_name = sys.argv[1]
+    try:
+        lines_without_empty_space = [line for line in Path(file_name).read_text().split('\n') if line]
+        line_count = len(lines_without_empty_space) -1
+        print(line_count)
+    except FileNotFoundError:
+        print(f"ERROR: '{file_name}' not found")
+
+
+
+if __name__ == '__main__':
+    main()
