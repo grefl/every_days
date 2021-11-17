be5b74ba2161300d2973d5350346e3c6dc2a1ffc
grefl <grefl@protonmail.com>
2021-11-17 18:03:03 +0100

    Separate logic, oop or oops?

--- cmd_line_scrippies/generic_sloccount.py
+++ cmd_line_scrippies/generic_sloccount.py
@@ -2,14 +2,32 @@ from pathlib import Path
 import sys
 from fetch import Request
 
-raw_url = "https://raw.githubusercontent.com"
 
+class RemoteGitFetch:
+    def __init__(self):
+        self.req = Request()
+        self.raw_url = "https://raw.githubusercontent.com"
+    
+    def fetch(self, url):
+        headers = {'Accept': 'application/json' }
+        normalized_url = self.normalize_url(url)
+        return self.req.get(normalized_url, {'data': None, 'headers': headers}) 
+
+    def normalize_url(self, url):
+        if 'github' in url:
+            end = ''.join(url.split('.com')[1].split('/blob'))
+            new_url = self.raw_url + end
+            return new_url
+
+        else: 
+            return url.replace('blob', 'raw')
 
 class CalcLines:
-    def __init__(self,file_name, type = 'fs'):
-        self.req = Request()
-        self.type = type
+    def __init__(self,file_name):
+        self.remote_fetch = RemoteGitFetch()
+        self.type = 'remote' if 'https' in file_name else 'fs' 
         self.file_name = file_name
+
     def get_file_string(self, name):
             try:
                return Path(name).read_text()
@@ -17,9 +35,7 @@ class CalcLines:
                 return f"ERROR: '{file_name}' not found"
 
     def get_file_from_web(self, url):
-        headers = {'Accept': 'application/json' }
-        normalized_url = self.normalize_url(url)
-        return self.req.get(normalized_url, {'data': None, 'headers': headers}) 
+        return self.remote_fetch.fetch(url) 
 
     def normalize_url(self, url):
         if self.type == 'github':
@@ -42,11 +58,11 @@ class CalcLines:
 
 
     def sloccount(self):
-        if self.type not in ['fs', 'github', 'gitlab']:
+        if self.type not in ['remote', 'fs']:
             raise Error(f"{self.type} is not a valid type")
 
         string = None
-        if self.type in ['github', 'gitlab']:
+        if self.type == 'remote':
             string = self.get_file_from_web(self.file_name)
         else:
             string = self.get_file_string(self.file_name)
@@ -55,6 +71,7 @@ class CalcLines:
             raise Error("string is None")
         line_count = self.calc_lines(string)
         return line_count
-url = 'https://gitlab.redox-os.org/redox-os/redox/-/blob/master/docker/Dockerfile'        
-calc = CalcLines(type='gitlab', file_name = url)
-assert calc.sloccount() == 39
+
+url = 'https://github.com/grefl/every_days/blob/master/my_python_libs/get_file.py'        
+calc = CalcLines(file_name = url)
+print(calc.sloccount())
