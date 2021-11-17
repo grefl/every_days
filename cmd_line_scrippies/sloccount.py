4dd012efcd78cf841088e0d4c9e8955af2c3b1b2
grefl <grefl@protonmail.com>
2021-11-17 18:10:27 +0100

    remove unnecessary -1

--- cmd_line_scrippies/sloccount.py
+++ cmd_line_scrippies/sloccount.py
@@ -6,7 +6,7 @@ def main():
     file_name = sys.argv[1]
     try:
         lines_without_empty_space = [line for line in Path(file_name).read_text().split('\n') if line]
-        line_count = len(lines_without_empty_space) -1
+        line_count = len(lines_without_empty_space) 
         print(line_count)
     except FileNotFoundError:
         print(f"ERROR: '{file_name}' not found")
