a91426d22220d25fcfd96a4c1858dbcfa2499e6d
grefl <grefl@protonmail.com>
2021-11-20 17:13:36 +0100

    whoops, forgot to add request

--- cmd_line_scrippies/search.py
+++ cmd_line_scrippies/search.py
@@ -1,6 +1,35 @@
-from main import Request
 from html.parser import HTMLParser
 import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+class Request():
+  def __init__(self):
+    self.method = 'GET'
+    self.headers = {}
+    self.data = {}
+    self.params = {}
+    
+ 
+
+  def get(self, url, options = {}): 
+    if not options:
+        options['data'] = None
+        options['headers'] = {'Accept': 'text/plain'}
+    req = urllib.request.Request(
+      url,
+      data=options['data'],
+      headers=options['headers'],
+      method=self.method
+    )
+    try:
+      
+      with urllib.request.urlopen(req) as res:
+        return res.read().decode(res.headers.get_content_charset('utf-8'))
+    except urllib.error.HTTPError as e:
+      print(e)
+
 
 class MyParser(HTMLParser):
   def init(self):
