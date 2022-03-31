afa2939d705732f0d67e853be4c31b852f4b818e
grefl <grefl@protonmail.com>
2022-03-31 23:03:57 +0200

    add git::head

--- my_python_libs/simple_git.py
+++ my_python_libs/simple_git.py
@@ -10,12 +10,15 @@ def check_for_git(path):
         return True, Path(git_path[0]) 
     else:
         return check_for_git(Path(path.parent))
+def head(path):
+    return Path(str(path) + '/HEAD').read_text().split('/').pop().rstrip()
 def main():
     is_git, path = check_for_git(Path('.').cwd())
     if is_git:
         branches = Path(str(path) + '/refs/heads').iterdir()
         for branch in branches:
             print(branch.name)
+        print(head(path))
         return
     print("[ERROR] no .git found.")
 
